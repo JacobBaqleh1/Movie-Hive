176c07c5b63810bd624c38e9ee155c6ab7a59347
Jacob Baqleh <101436252+JacobBaqleh1@users.noreply.github.com>
2023-04-17 20:27:16 -0700

    romeved next from gitignore

---
+++ movie-hive/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\jacob\\Movie-Hive\\Movie-Hive\\movie-hive\\app\\page.tsx": [
+      "C:\\Users\\jacob\\Movie-Hive\\Movie-Hive\\movie-hive\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\jacob\\Movie-Hive\\Movie-Hive\\movie-hive\\app\\layout.tsx": [
+      "C:\\Users\\jacob\\Movie-Hive\\Movie-Hive\\movie-hive\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\jacob\\Movie-Hive\\Movie-Hive\\movie-hive\\app\\page": [
+      "C:\\Users\\jacob\\Movie-Hive\\Movie-Hive\\movie-hive\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\jacob\\Movie-Hive\\Movie-Hive\\movie-hive\\app\\globals.css"
+    ]
+  }
+}
